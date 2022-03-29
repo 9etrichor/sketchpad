06408f13e4fd1ab32167d56ff0502a9ef41fb06b
9etrichor <dick2452683247@gmail.com>
2022-03-29 11:46:52 +0800

    can create elements with correct sizes and numbers of elements

--- sketchpad.js
+++ sketchpad.js
@@ -1,1 +1,24 @@
+let size = 16; /*number of box each side */
+let sideLength = 512 / size; /* size of a single box */
+let colDiv; /* store column element just create */
+let rowDiv; /* store row element just create */
 const container = document.getElementById('container');
+
+/* create elements start */
+for(let i = 0;i < size;i++) //make column elements
+{
+    colDiv = document.createElement('div');
+    colDiv.classList.add('a_row');
+    colDiv.style.height = `${sideLength}px`; //set every column elements' height to fill height of container
+    container.appendChild(colDiv);
+
+    for(let r = 0; r < size; r++) //make row elements, paraent is column element
+    {
+        rowDiv = document.createElement('div');
+        colDiv.appendChild(rowDiv);
+        rowDiv.textContent = "1";
+        rowDiv.style.width = `${sideLength}px`; //set every row elements' width to fill width of container
+    };
+};
+/* create elements end */
+console.log();
